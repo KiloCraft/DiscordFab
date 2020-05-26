381de8f9cd159290986841f4b30443403811e3de
CODY_AI <cody_ai@outlook.com>
2020-05-26 21:55:39 +0430

    Better ping command

---
+++ src/main/java/com/github/hansi132/discordfab/discordbot/util/Constants.java
@@ -0,0 +1,7 @@
+package com.github.hansi132.discordfab.discordbot.util;
+
+import java.text.DecimalFormat;
+
+public class Constants {
+    public static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("##.##");
+}
